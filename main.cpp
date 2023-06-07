93825a36f92a4d3db0fc0ca01774a183bfcff158
Rob Parry <58118390+ROB3RT0-P@users.noreply.github.com>
2023-06-07 12:25:54 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include "global.h"
+#include "game.h"
+#include "processmanager.h"
+#include "entitymanager.h"
+#include "resourcemanager.h"
+
+//Using SDL, SDL_image, standard IO, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+namespace global
+{
+namespace // hidden namespace
+{
+	Game* GameInstance;
+	ProcessManager* ProcessManagerInstance;
+	EntityManager* EntityManagerInstance;
+	ResourceManager* ResourceManagerInstance;
+
+
+	ResourceManager::ResourceDBEntry resourceDatabase[] =
+	{
+		{ raw_enum(Res::Default),			"texture.png",								ResourceManager::ResourceType::Texture },
+		{ raw_enum(Res::PlayerSprite),		"kenny/Ships/ship_0001.png",				ResourceManager::ResourceType::Texture },
+	};
+}
+	Game* game() { return GameInstance; }
+	ProcessManager* processManager() { return ProcessManagerInstance; }
+	EntityManager* entityManager() { return EntityManagerInstance; }
+	ResourceManager* resourceManager() { return ResourceManagerInstance; }
+}
+
+int main( int argc, char* args[] )
+{
+	global::GameInstance = new Game();
+	global::EntityManagerInstance = new EntityManager();
+	global::ProcessManagerInstance = new ProcessManager();
+	global::ResourceManagerInstance = new ResourceManager();
+
+	global::resourceManager()->initializeResourceDatabase(global::resourceDatabase, sizeof(global::resourceDatabase) / sizeof(global::resourceDatabase[0]), raw_enum(global::Res::Max), "./Data/");
+
+	//Start up SDL and create window
+	if( !global::game()->initialize(640, 480))
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		{	
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while( !quit )
+			{
+				//Handle events on queue
+				while( SDL_PollEvent( &e ) != 0 )
+				{
+					//User requests quit
+					if( e.type == SDL_QUIT )
+					{
+						quit = true;
+					}
+				}
+
+				global::game()->tickLogic();
+
+				global::game()->renderAndPresent();
+
+				global::game()->postFrameUpdate();
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	global::game()->close();
+
+	return 0;
+}
